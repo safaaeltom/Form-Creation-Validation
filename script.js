033f64372242fbcf98d3ed36a4d7e31512feb324
Safaa <safaaeltom@gmail.com>
2025-12-02 20:15:58 +0300

    updated correct javascript codelines to repo

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    const form = document.getElementById("registeration-form");
+    const form = document.getElementById("registration-form");
     const feedbackDiv = document.getElementById("form-feedback");
      form.addEventListener("submit", function(event) {
         event.preventDefault();
 
 
-    const username = document.getElementById("username").value.trim;
-    const email = document.getElementById("email").value.trim;
-    const password = document.getElementById("password").value.trim;
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
 
 
 let isValid = true;
